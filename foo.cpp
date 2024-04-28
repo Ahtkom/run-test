75d3af0c6da77b88b317bf7640520ad36301f0dd
Ahtkom <80094892+Ahtkom@users.noreply.github.com>
2024-04-28 13:30:20 +0800

    Update foo.cpp

--- foo.cpp
+++ foo.cpp
@@ -1,4 +1,28 @@
-#include <iostream>
+#include "geos/geom/Geometry.h"
+#include "geos/io/WKTReader.h"
+#include "geos/operation/buffer/BufferOp.h"
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace geos;
+using namespace geom;
+using namespace io;
+using namespace operation;
+
+extern "C" {
+
+void test_geos_utility() {
+    WKTReader reader;
+
+    auto g          = reader.read("LINESTRING(10 10,20 20,100 90)");
+    auto g_buffered = buffer::BufferOp::bufferOp(g.get(), 3);
+
+    std::ofstream ofs("./geos-test.txt");
+    ofs << g_buffered->toString();
+}
+
+}
 
 
